e7769e66857d781dc84b7f01ea3701f4239a2931
agnij-workwise <agnij@letsworkwise.com>
2025-06-24 21:18:03 +0530

    feat: Add bulk mapping functionality to Private Vendor Management

---
+++ pages/private-vendor-management/bulk-mapping/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import BulkBuyerVendorMapping from "@/components/private-vendor-management/bulk-mapping";
+
+const BulkBuyerVendorMappingPage = () => {
+  return (
+      <BulkBuyerVendorMapping />
+  );
+};
+
+export default BulkBuyerVendorMappingPage; 
