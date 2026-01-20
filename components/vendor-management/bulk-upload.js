7aeb61b4f38982ea27e4a71c55ffb9fec02af2c4
sarvesh-ctrl <sarvesh@letsworkwise.com>
2026-01-20 06:27:12 +0530

    Changed the name of env variable.

--- components/vendor-management/bulk-upload.js
+++ components/vendor-management/bulk-upload.js
@@ -41,7 +41,7 @@ const BulkVendorUpload = () => {
 
   const downloadSampleFile = () => {
     const link = document.createElement('a');
-    link.href = process.env.NEXT_PUBLIC_Bulk_UPLOAD_FILE_PATH;
+    link.href = process.env.NEXT_PUBLIC_BULK_UPLOAD_FORMAT;
     link.download = 'Bulk_Upload_Format.xlsx';
     document.body.appendChild(link);
     link.click();
