ddf7c57cd652e9d9f26f1bff29329f83432af4aa
sarvesh-ctrl <sarvesh@letsworkwise.com>
2026-03-09 20:06:46 +0530

    Created 'Feedback' page.

---
+++ utils/services/feedback.ts
@@ -0,0 +1,32 @@
+import axiosInstance from "../axios";
+
+interface FeedbackFilters {
+    action_key?: string;
+    event?: string;
+    start_date?: string;
+    end_date?: string;
+}
+
+export const handleGetFeedbackList = (page = 1, limit = 10, filters?: FeedbackFilters) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let url = `${process.env.NEXT_PUBLIC_API_WEB_URL}/admin/monitoring/feedback-list?page=${page}&limit=${limit}`;
+            if (filters?.action_key) {
+                url += `&action_key=${filters.action_key}`;
+            }
+            if (filters?.event) {
+                url += `&event=${filters.event}`;
+            }
+            if (filters?.start_date) {
+                url += `&start_date=${filters.start_date}`;
+            }
+            if (filters?.end_date) {
+                url += `&end_date=${filters.end_date}`;
+            }
+            let response = await axiosInstance.get(url);
+            resolve(response);
+        } catch (error) {
+            reject({ error });
+        }
+    });
+};
